b89f30e8a9caf9f00da393c306858f7c6e81b85f
Faris Khodaeh <88697157+Faris014@users.noreply.github.com>
2021-10-23 23:18:44 +0700

    Delete myapp directory

--- myapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from rest_framework import serializers
-from .models import Todolist
-
-class TodolistSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Todolist
-        fields = ('id','title','detail') # '__all__'
